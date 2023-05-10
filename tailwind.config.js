c80d2157f53c702206eb53c7e8a53d73cbc254a7
sarfarazstark <sarfarazstark@gmail.com>
2023-05-10 13:48:27 +0530

    Fix tailwindcss ❗ Added new assets ✅ Completed music list sidebar 💹

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["**/*.{html,js}"],
+  content: ["./index.html", "./assets/script.js"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+          "phthalo-blue": "hsla(242, 70%, 29%, 1)",
+          "cornflower-blue": "hsla(221, 100%, 66%, 1)",
+          "palatinate-blue": "hsla(241, 97%, 60%, 1)",
+          "french-rose": "hsla(342, 100%, 62%, 1)",
+          "penn-blue": "hsla(239, 77%, 12%, 1)",
+          white: "rgb(255 255 255)",
+        },
+        fontFamily: {
+          "Josefin": "Josefin Sans, sans-serif",
+          "Kanit": "Kanit, sans-serif",
+        },
+        right:{
+          '4px': '4px'
+        }
+    },
   },
+  extend: {},
   plugins: [],
-}
-
+};
