4a89aac497170c400d4f2489a5400e8ecf6e9af4
sarfarazstark <sarfarazstark@gmail.com>
2023-05-13 13:19:57 +0530

    UPDATE: completed controller ui added fontawesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,13 @@ module.exports = {
   theme: {
     extend: {
         colors: {
+          "penn-blue-5": "rgba(7, 8, 54, 0.511)",
           "phthalo-blue": "hsla(242, 70%, 29%, 1)",
           "cornflower-blue": "hsla(221, 100%, 66%, 1)",
           "palatinate-blue": "hsla(241, 97%, 60%, 1)",
           "french-rose": "hsla(342, 100%, 62%, 1)",
           "penn-blue": "hsla(239, 77%, 12%, 1)",
-          white: "rgb(255 255 255)",
+          "white": "rgb(255 255 255)",
         },
         fontFamily: {
           "Josefin": "Josefin Sans, sans-serif",
