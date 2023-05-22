15bb238fabaad702622a7973010bbf7b61914a6c
Md Sarfaraz Alam <sarfarazstark@gmail.com>
2023-05-22 22:15:13 +0530

    Redoing the design with mobile first

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 const wrapper = document.querySelector(".wrapper")
 const musics = wrapper.querySelector(".preset-music");
 function playMusic() {
-    const audio = new Audio(allmusic[2].musics[0]);
+    const audio = new Audio(allMusic[2].musics[0]);
     console.log(audio);
     audio.play();
 }
